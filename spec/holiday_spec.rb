1e5ffea241206cd3c5c4cc90f2c4ca2dd1648948
Katherine Gruenberg <katherine.gruenberg@gmail.com>
2017-10-04 17:30:16 +0000

    automatically backed up by learn

--- spec/holiday_spec.rb
+++ spec/holiday_spec.rb
@@ -13,7 +13,7 @@ describe "holiday_supplies_hash" do
         :thanksgiving => ["Turkey"]
       },
       :spring => {
-        :memorial_day => ["BBQ", "Grill", "Table Cloth"]
+        :memorial_day => ["BBQ"]
       }
     }
   }
